9c23d607463ea2efe5d38b13bb421c0cc3f711d4
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-28 10:18:31 +0530

    forms-component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent, FavoriteComponent, LikeComponent, InputFormatDirective, ZippyComponent],
+  declarations: [AppComponent, CoursesComponent, CourseComponent, FavoriteComponent, LikeComponent, InputFormatDirective, ZippyComponent, ContactFormComponent],
   imports: [BrowserModule, FormsModule],
   providers: [CoursesService],
   bootstrap: [AppComponent],
